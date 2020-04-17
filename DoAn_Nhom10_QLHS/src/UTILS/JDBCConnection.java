a1f593d5c111f98096eb16fb9aa16158c9dee8a1
evel2903 <evel2903>
2020-04-17 15:46:25 +0700

    init project

---
+++ DoAn_Nhom10_QLHS/src/UTILS/JDBCConnection.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UTILS;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hunte
+ */
+public class JDBCConnection {
+    public static Connection getJDBCConnection(){
+        final String url = "jdbc:mysql://localhost:3306/user";
+        final String user = "root";
+        final String password = "admin"; 
+          
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            return DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(JDBCConnection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(JDBCConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
